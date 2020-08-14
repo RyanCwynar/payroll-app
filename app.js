410c5249494b51b263734e32f7bf433616feebdf
Ryan Cwynar <ryan@skulabs.com>
2020-08-13 20:19:18 -0400

    add some more endpoints

--- app.js
+++ app.js
@@ -32,15 +32,57 @@ app.all("/summary", async (req, res) => {
     cache.set("summary", summary);
   }
 });
+app.post("/ceiling", async (req, res) => {
+  if (cache.has("summary")) {
+    const summary = cache.get("summary")
+    res.send(`There are ${summary.ceiling_hours} available from FoB this week`)
+  } else {
+    res.send("check back in a minute");
+    let summary = await WooData.getSummary();
+    cache.set("summary", summary);
+  }
+});
 
-app.post("/test", (req, res) => {
+app.post("/hours", (req, res) => {
   let { user_name, text } = req.body;
   weeklyHours[user_name] = Number(text);
   res.json({ weeklyHours, totalHours: getTotalWeeklyHours() });
 });
+app.post("/add-hours", (req, res) => {
+  let { user_name, text } = req.body;
+  if(weeklyHours[user_name])
+    weeklyHours[user_name] += Number(text);
+  else
+    weeklyHours[user_name] = Number(text);
+  res.send(`You're at ${weeklyHours[user_name]} so far this week`);
+});
 
-app.post("/total-hours", (req, res) => {
-  res.json({ totalHours: getTotalWeeklyHours() });
+app.post("/current-hours", (req, res) => {
+  res.send(`Total actual hours report this week ${getTotalWeeklyHours()}`);
+});
+
+app.post("/my-payout", async (req, res) => {
+  const { user_name } = req.body
+  let hours = Number(weeklyHours[user_name])
+  if(!hours){
+    res.send("You didn't report any hours yet")
+    return
+  }
+  const summary = cache.get("summary")
+  const availableHours = (summary) ? summary.ceiling_hours : 0
+  if(!availableHours){
+    res.send("check back in a minute");
+    let summary = await WooData.getSummary();
+    cache.set("summary", summary);
+    return
+  }
+  const actualHours = getTotalWeeklyHours()
+  
+  if(availableHours < actualHours){
+    hours = hours / actualHours * availableHours
+  }
+  const payout = Number(hours * 50).toFixed(2)
+  res.send(`Projected payout: $${payout}`)
 });
 
 // Error handler
